9f4183aaaaffaa2cc866cb16a6de3f86feba020a
xiaokunxu <996670910@qq.com>
2017-04-27 17:57:20 +0800

    modify在注册和登录表单之间切换

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -14,71 +14,76 @@ export default class UserDialog extends Component {
         })
     }
     render() {
-        let signUpForm = ( <
-            label > <
-            input type = "radio"
-            value = "signUp"
-            checked = { this.state.selected === 'signUp' }
-            /> 注册 </label >
+        let signUpForm = (
+            <
+             form className = "signUp" > { /*注册*/ } <
+             div className = "row" >
+             <
+             label > 用户名 < /label> <
+             input type = "text" / >
+             <
+             /div> <
+             div className = "row" >
+             <
+             label > 密码 < /label> <
+             input type = "password" / >
+             <
+             /div> <
+             div className = "row actions" >
+             <
+             button type = "submit"
+             className = "btn" > 注册 < /button> < /
+             div > <
+             /form>
         )
-        let signInForm = ( <
-            label > <
-            input type = "radio"
-            value = "signIn"
-            checked = { this.state.selected === 'signIn' }
-            / > 登录 </label >
+        let signInForm = (
+            <
+             form className = "signIn" > { /*登录*/ } <
+             div className = "row" >
+             <
+             label > 用户名 < /label> <
+             input type = "text" / >
+             <
+             /div> <
+             div className = "row" >
+             <
+             label > 密码 < /label> <
+             input type = "password" / >
+             <
+             /div> <
+             div className = "row actions" >
+             <
+             button type = "submit"
+             className = "btn" > 登录 < /button> < /
+             div > <
+             /form> 
         )
-        return ( <
-            div className = "UserDialog-Wrapper" >
-            <
-            div className = "UserDialog" >
-            <
-            nav onChange = { this.switch.bind(this) } >
-            <
-            /
-            nav > <
-            div className = "panes" > { this.state.selected === 'signUp' ? signUpForm : null } { this.state.selected === 'signIn' ? signInForm : null } <
-            form className = "signUp" > { /*注册*/ } <
-            div className = "row" >
-            <
-            label > 用户名 < /label> <
-            input type = "text" / >
-            <
-            /div> <
-            div className = "row" >
-            <
-            label > 密码 < /label> <
-            input type = "password" / >
-            <
-            /div> <
-            div className = "row actions" >
-            <
-            button type = "submit"
-            className = "btn" > 注册 < /button> < /
-            div > <
-            /form> <
-            form className = "signIn" > { /*登录*/ } <
-            div className = "row" >
-            <
-            label > 用户名 < /label> <
-            input type = "text" / >
-            <
-            /div> <
-            div className = "row" >
-            <
-            label > 密码 < /label> <
-            input type = "password" / >
-            <
-            /div> <
-            div className = "row actions" >
-            <
-            button type = "submit"
-            className = "btn" > 登录 < /button> < /
-            div > <
-            /form> < /
-            div > <
-            /div> < /
-            div >
-        )
-    }
-}
+         return ( <
+             div className = "UserDialog-Wrapper" >
+             <
+             div className = "UserDialog" >
+              <
+              nav onChange = { this.switch.bind(this) } >
+              <
+              label > <
+              input type = "radio"
+              value = "signUp"
+              checked = { this.state.selected === 'signUp' }
+              /> 注册 </label >
+              <
+              label > <
+              input type = "radio"
+              value = "signIn"
+              checked = { this.state.selected === 'signIn' }
+              / > 登录 </label > < /
+              nav > <
+              div className = "panes" >
+              {this.state.selected === 'signUp' ? signUpForm : null}
+              {this.state.selected === 'signIn' ? signInForm : null}
+               < /
+             div > <
+             /div> < /
+             div >
+         )
+     }
+ }
