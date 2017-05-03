e661dee062fced0e3dc568f8a240b7d9a8b4afd3
xiaokunxu <996670910@qq.com>
2017-05-03 17:39:49 +0800

    消除 "需要 onChange" 的警告

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -88,30 +88,29 @@ export default class UserDialog extends Component {
             div > <
             /form> 
         )
-        return ( <
-            div className = "UserDialog-Wrapper" >
-            <
-            div className = "UserDialog" >
-            <
-            nav onChange = { this.switch.bind(this) } >
-            <
-            label > <
-            input type = "radio"
-            value = "signUp"
-            checked = { this.state.selected === 'signUp' }
-            /> 注册 </label >
-            <
-            label > <
-            input type = "radio"
-            value = "signIn"
-            checked = { this.state.selected === 'signIn' }
-            / > 登录 </label > < /
-            nav > <
-            div className = "panes" > { this.state.selected === 'signUp' ? signUpForm : null } { this.state.selected === 'signIn' ? signInForm : null } <
-            /
-            div > <
-            /div> < /
-            div >
+        return ( 
+            <div className = "UserDialog-Wrapper">
+                <div className = "UserDialog" >
+                    <nav>
+                        <label>
+                            <input type="radio" value="signUp" 
+                                checked={this.state.selected === 'signUp'} 
+                                onChange={this.switch.bind(this)}
+                            />注册
+                        </label>
+                        <label>
+                            <input type="radio" value="signIn" 
+                                checked={this.state.selected === 'signIn'} 
+                                onChange={this.switch.bind(this)}
+                            />登录
+                        </label>
+                    </nav>
+                    <div className = "panes"> 
+                        { this.state.selected === 'signUp' ? signUpForm : null } 
+                        { this.state.selected === 'signIn' ? signInForm : null } 
+                    </div> 
+                </div> 
+            </div>
         )
     }
 }
