738b04e05a05d737ab039c4e36f4c7fd88c532d0
xiaokunxu <996670910@qq.com>
2017-05-03 23:16:36 +0800

    合并 onSignUp 和 onSignIn

--- src/App.js
+++ src/App.js
@@ -50,8 +50,8 @@ class App extends Component {
             {this.state.user.id ? 
                 null : 
                 <UserDialog 
-                onSignUp = { this.onSignUp.bind(this) }
-                onSignIn = { this.onSignIn.bind(this) }/>}
+                onSignUp = { this.onSignUpOrSignIn.bind(this) }
+                onSignIn = { this.onSignUpOrSignIn.bind(this) }/>}
             </div>
         )
     }
@@ -61,12 +61,7 @@ class App extends Component {
         stateCopy.user = {}
         this.setState(stateCopy)
     }
-    onSignUp(user) {
-        let stateCopy = JSON.parse(JSON.stringify(this.state))
-        stateCopy.user = user
-        this.setState(stateCopy)
-    }
-    onSignIn(user){
+    onSignUpOrSignIn(user){
         let stateCopy = JSON.parse(JSON.stringify(this.state))
         stateCopy.user = user
         this.setState(stateCopy)
